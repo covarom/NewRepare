ae4fc2f8a70e21236c7af24f5cda030110189e2d
covarom <huudoan.qna@gmail.com>
2022-08-26 09:06:02 +0700

    check

---
+++ SE150557_NguyenHuuDoan/Managerment.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SE150557_NguyenHuuDoanRepo.BussinessObject.Models;
+using SE150557_NguyenHuuDoanRepo.DataAccess.Repository;
+
+namespace SE150557_NguyenHuuDoan
+{
+    public partial class Managerment : Form
+    {
+        public Managerment()
+        {
+            InitializeComponent();
+        }
+        BindingSource source;
+
+        ICustomerRepo customerRepo = new CustomerRepo();
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void LoadUserList(IEnumerable<BankAccount> cs)
+        {
+            try
+            {
+                txtAccountId.Text = null;
+                txtAccountName.Text = null;
+                txtBranchName.Text = null;
+                txtOpenDate.Text = null;
+                txtTypeID.Text = null;
+                
+
+                source = new BindingSource();
+
+
+                source.DataSource = cs.ToList();
+
+                txtAccountId.DataBindings.Clear();
+                txtAccountName.DataBindings.Clear();
+                txtBranchName.DataBindings.Clear();
+                txtOpenDate.DataBindings.Clear();
+                txtTypeID.DataBindings.Clear();
+
+                txtAccountId.DataBindings.Add("Text", source, "AccountID");
+                txtAccountName.DataBindings.Add("Text", source, "AccountName");
+                txtBranchName.DataBindings.Add("Text", source, "BranchName");
+                txtOpenDate.DataBindings.Add("Text", source, "OpenDate");
+                txtTypeID.DataBindings.Add("Text", source, "TypeID");
+
+
+                dgvAccount.DataSource = source;
+                if(dgvAccount.Columns["Type"] != null)
+                {
+                    dgvAccount.Columns["Type"].Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadUserList(customerRepo.GetAll());
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            NewForm formInsert = new NewForm()
+            {
+                Text = " Thêm khách hàng mới",
+                InsertOrUpdate = false,
+                customerRepo = customerRepo,
+                mn = this
+            };
+            if (formInsert.ShowDialog() == DialogResult.OK)
+            {
+                LoadUserList(customerRepo.GetAll());
+                source.Position = source.Position - 1;
+            }
+            LoadUserList(customerRepo.GetAll());
+        }
+        public BankAccount getCustomer()
+        {
+            BankAccount cs = null;
+            try
+            {
+
+                cs = new BankAccount()
+                {
+                    AccountId = txtAccountId.Text,
+                    AccountName = txtAccountName.Text,
+                    BranchName = txtBranchName.Text,
+                    OpenDate = DateTime.Parse(txtOpenDate.Text),
+                    TypeId = txtTypeID.Text,
+
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error: " + ex.Message);
+            }
+            return cs;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            NewForm formInsert = new NewForm()
+            {
+                Text = "Cập nhật thông tin khách hàng",
+                InsertOrUpdate = true,
+                cs = getCustomer(),
+                customerRepo = customerRepo,
+                mn = this
+            };
+            if (formInsert.ShowDialog() == DialogResult.OK)
+            {
+                LoadUserList(customerRepo.GetAll());
+                source.Position = source.Position - 1;
+            }
+            LoadUserList(customerRepo.GetAll());
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var cs = getCustomer();
+                customerRepo.Delete(cs);
+                MessageBox.Show("Xoá thành công !");
+                LoadUserList(customerRepo.GetAll());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void Managerment_Load(object sender, EventArgs e)
+        {
+            LoadUserList(customerRepo.GetAll());
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var name = txtSearch.Text;
+            if (name == " ")
+            {
+                MessageBox.Show("Không để trống !");
+            }
+            else
+            {
+                LoadUserList(customerRepo.Search(name));
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+    }
+}
---
+++ SE150557_NguyenHuuDoan/NewForm.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using SE150557_NguyenHuuDoanRepo.BussinessObject.Models;
+using SE150557_NguyenHuuDoanRepo.DataAccess.Repository;
+
+namespace SE150557_NguyenHuuDoan
+{
+    public partial class NewForm : Form
+    {
+        public NewForm()
+        {
+            InitializeComponent();
+        }
+        public BankAccount cs { get; set; }
+        public bool InsertOrUpdate { get; set; }
+
+        public ICustomerRepo customerRepo = new CustomerRepo();
+
+        public Managerment ? mn = null;
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        static bool checkCapital(string abc)
+        {
+            string[] listcheck = new string[10];
+
+            string[] listWord = abc.Split(" ");
+            for (int i = 0; i < listWord.Length; i++)
+            {
+                if (Char.IsUpper(listWord[i], 0))
+                {
+
+                    listcheck[i] = "a";
+                }
+                else
+                {
+                    listcheck[i] = "b";
+                }
+            }
+            var check = true;
+            if (Array.Exists(listcheck, element => element == "b"))
+            {
+                check = false;
+            }
+            return check;
+        }
+        /// New function
+        static bool digitNumber(string abc)
+        {
+            bool check = false;
+            if(abc.Length >=6 && abc.Length <= 12)
+            {
+                check = true;
+            }
+            return check;
+        }
+
+        static bool authorName(string abc)
+        {
+            bool check = false;
+            if(abc.Length >= 10)
+            {
+                check = true;
+            }
+            return check;
+        }
+
+        // End
+        static bool checkYear(DateTime abc)
+        {
+            Console.WriteLine(abc);
+            bool check = false;
+           
+            if(abc.Year <=2022 && abc.Year >= 2000)
+            {
+                check = true;
+            }
+            return check;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtAccountID.Text) || String.IsNullOrEmpty(txtAccountName.Text) 
+                || String.IsNullOrEmpty(txtBranchName.Text) || String.IsNullOrEmpty(txtOpenDate.Text)
+                || String.IsNullOrEmpty(txtTypeID.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+            }
+            else
+            {
+                if (digitNumber(txtAccountID.Text))
+                {
+
+
+                    if (DateTime.Parse(txtOpenDate.Text) <= DateTime.Today)
+                    {
+                        if (checkCapital(txtBranchName.Text))
+                        {
+
+                            if (checkYear(DateTime.Parse(txtOpenDate.Text)))
+                            {
+                                var BankAccount = new BankAccount()
+                                {
+                                    AccountId = txtAccountID.Text,
+                                    AccountName = txtAccountName.Text,
+                                    BranchName = txtBranchName.Text,
+                                    OpenDate = DateTime.Parse(txtOpenDate.Text),
+                                    TypeId = txtTypeID.Text,
+                                };
+                                if (InsertOrUpdate == false)
+                                {
+                                    customerRepo.Add(BankAccount);
+                                    MessageBox.Show("Thêm khách hàng thành công !!");
+                                    txtAccountID.Clear();
+                                    txtAccountName.Clear();
+                                    txtBranchName.Clear();
+                                    mn.LoadUserList(customerRepo.GetAll());
+                                }
+                                else
+                                {
+                                    customerRepo.Update(BankAccount);
+                                    MessageBox.Show("Cập nhật khách hàng thành công !!");
+                                    mn.LoadUserList(customerRepo.GetAll());
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Năm nằm trong khoản 2000 tới 2022");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Viết hoa chữ cái đầu của Branch Name !!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Open Date phải bé hơn hiện tại !!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("@@@@ !!");
+                }
+            }
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NewForm_Load(object sender, EventArgs e)
+        {
+            if (InsertOrUpdate == true)//update mode
+            {
+                txtAccountID.Text = cs.AccountId.ToString();
+                txtAccountName.Text = cs.AccountName.ToString();
+                txtBranchName.Text = cs.BranchName.ToString();
+                txtOpenDate.Text = cs.OpenDate.ToString();
+                txtTypeID.Text = cs.TypeId.ToString();
+               
+            }
+        }
+    }
+}
